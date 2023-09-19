49a0fa9c6a666f01d3edcaefe057321422061301
Giovanni Aguirre <gaguirre@2600hz.com>
2023-09-18 22:14:37 -0600

    Add project files.

---
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CategoryId { get; set; }
+        // M:M Relation
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+    }
+}
