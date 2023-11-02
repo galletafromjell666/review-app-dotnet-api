3ef27a2f595c86bd6463ffd91fde27b361be96ca
Giovanni Aguirre <gaguirre@2600hz.com>
2023-11-01 21:53:56 -0600

    Add review owner and controller

---
+++ PokemonReviewApp/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetPokemonOwner(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+
+    }
+}
